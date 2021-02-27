618e636db3e45f96e57d184f4ebf3e356216f823
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2021-02-27 09:48:08 -0600

    Adding Edit and Delete functions to Products and Services

--- assignment1p2/mfscrm/crm/forms.py
+++ assignment1p2/mfscrm/crm/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Customer, Service
+from .models import Customer, Product, Service
 
 class CustomerForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,8 @@ class ServiceForm(forms.ModelForm):
    class Meta:
        model = Service
        fields = ('cust_name', 'service_category', 'description', 'location', 'setup_time', 'cleanup_time', 'service_charge' )
+       
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = ('cust_name', 'product', 'p_description', 'quantity', 'pickup_time', 'charge')
